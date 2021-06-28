61d26fc1465a13f5cf73dfb5f7eecec3fe81e66f
Brian Yu <brianyu28@gmail.com>
2021-06-28 13:55:17 -0400

    Section 2

---
+++ initials.c
@@ -0,0 +1,10 @@
+// Write a program that prints the initials of a name provided as command-line arguments.
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    // TODO
+}
+
---
+++ reverse.c
@@ -0,0 +1,8 @@
+// Write a program that takes 5 integers and prints them in reverse order.
+
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO
+}
